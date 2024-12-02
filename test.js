1dfdea7a3168d5b1aa9975b7f0b2f65bea95f99a
andzejs <andzejs@maksekeskus.ee>
2024-12-02 22:59:41 +0200

    FEAT-2 - updated lv swearwords  :D

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const { Automod, Lang, Method } = require('./lib/index.js');
 
 console.log(Automod.filter("d@mn а я fuck не понял 6ля что вы shit делаете whore в холодильнике cуk@", { langs: [ Lang.ENGLISH ] }))
-console.log(Automod.filter("d@mn а я fuck не понял 6ля что вы shit делаете whore в холодильнике cуk@ сабля, манда мандарин bāba", { langs: [ Lang.ENGLISH, Lang.RUSSIAN, Lang.LATVIAN ], method: Method.STRICT }))
+console.log(Automod.filter("d@mn а я fuck не понял 6ля что вы shit делаете whore в pidars холодильнике cуk@ сабля, манда мандарин bāba", { langs: [ Lang.ENGLISH, Lang.RUSSIAN, Lang.LATVIAN ], method: Method.STRICT }))
