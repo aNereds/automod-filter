0c4e3ae1f3b744754d46c09dd02ada8114567ca4
andzejs <andzejs@maksekeskus.ee>
2024-12-02 21:30:05 +0200

    Initial commit

--- src/structures/Filter.ts
+++ src/structures/Filter.ts
@@ -2,100 +2,128 @@ import ru from "../data/ru.json";
 import en from "../data/en.json";
 import { AutomodOptions, AutomodReply } from "../interfaces";
 
-export enum Lang { ENGLISH, RUSSIAN };
-export enum Method { CLASSIC, STRICT };
+export enum Lang {
+  ENGLISH,
+  RUSSIAN,
+}
+export enum Method {
+  CLASSIC,
+  STRICT,
+}
 
 export class Filter {
+  readonly defaultOpts: AutomodOptions = {
+    langs: [Lang.ENGLISH],
+    replacer: "█",
+    method: Method.CLASSIC,
+  };
+  private readonly methods: Map<Method, Function> = new Map();
 
-    readonly defaultOpts: AutomodOptions = {
-        langs: [ Lang.ENGLISH ],
-        replacer: "█",
-        method: Method.CLASSIC
+  constructor() {
+    this.methods
+      .set(Method.CLASSIC, (word: string, langs: Lang[]): boolean => {
+        if (
+          langs.includes(Lang.RUSSIAN) &&
+          !ru.exclude.includes(word) &&
+          ru.include.filter((w) => word === w || word.includes(w)).length != 0
+        ) {
+          return true;
+        }
+        if (
+          langs.includes(Lang.ENGLISH) &&
+          !en.exclude.includes(word) &&
+          en.include.filter((w) => word === w || word.includes(w)).length != 0
+        ) {
+          return true;
+        }
+        return false;
+      })
+      .set(Method.STRICT, (word: string, langs: Lang[]): boolean => {
+        const results: boolean[] = [];
+        if (langs.includes(Lang.RUSSIAN) && !ru.exclude.includes(word)) {
+          if (
+            ru.include.filter((w) => word === w || word.includes(w)).length != 0
+          ) {
+            return true;
+          }
+          results.push(this.found(word, ru));
+        }
+        if (langs.includes(Lang.ENGLISH) && !en.exclude.includes(word)) {
+          if (
+            en.include.filter((w) => word === w || word.includes(w)).length != 0
+          ) {
+            return true;
+          }
+          results.push(this.found(word, en));
+        }
+        return results.includes(true);
+      });
+  }
+
+  /**
+   * @param  { string } string Query string to filter
+   * @param  { AutomodOptions } options Filter options { langs: Lang[], replacer: "█", method: Method }
+   * @returns { AutomodReply } Output data { clean: boolean, output: string, ... }
+   */
+  filter(
+    string: string = "",
+    options: AutomodOptions = this.defaultOpts,
+  ): AutomodReply {
+    const { langs, replacer, method } = { ...this.defaultOpts, ...options };
+    let reply: AutomodReply = {
+      time: Date.now(),
+      input: string,
+      output: string,
+      matches: [],
+      clean: true,
     };
-    private readonly methods: Map<Method, Function> = new Map();
+    reply = this.clean(reply, { langs, replacer, method });
+    reply.time = Date.now() - reply.time;
+    return reply;
+  }
 
-    constructor() {
-        this.methods
-            .set(Method.CLASSIC, (word: string, langs: Lang[]): boolean => {
-                if (langs.includes(Lang.RUSSIAN) && !ru.exclude.includes(word) && ru.include.filter((w) => word === w || word.includes(w)).length != 0) {
-                    return true;
-                }
-                if (langs.includes(Lang.ENGLISH) && !en.exclude.includes(word) && en.include.filter((w) => word === w || word.includes(w)).length != 0) {
-                    return true;
-                }
-                return false;
-            })
-            .set(Method.STRICT, (word: string, langs: Lang[]): boolean => {
-                let results: boolean[] = [];
-                if (langs.includes(Lang.RUSSIAN) && !ru.exclude.includes(word)) {
-                    if (ru.include.filter((w) => word === w || word.includes(w)).length != 0) {
-                        return true;
-                    }
-                    results.push(this.found(word, ru));
-                }
-                if (langs.includes(Lang.ENGLISH) && !en.exclude.includes(word)) {
-                    if (en.include.filter((w) => word === w || word.includes(w)).length != 0) {
-                        return true;
-                    }
-                    results.push(this.found(word, en));
-                }
-                return results.includes(true);
-            });
+  private clean(reply: AutomodReply, options: AutomodOptions): AutomodReply {
+    const isProfane = this.methods.get(options.method);
+    for (const word of reply.output.split(/ /)) {
+      if (isProfane ? isProfane(word, options.langs) : false) {
+        reply.matches.push(word);
+        reply.clean = false;
+        reply.output = reply.output.replace(
+          word,
+          options.replacer.repeat(word.length),
+        );
+      }
     }
-    
-	/**
-	 * @param  { string } string Query string to filter
-	 * @param  { AutomodOptions } options Filter options { langs: Lang[], replacer: "█", method: Method }
-	 * @returns { AutomodReply } Output data { clean: boolean, output: string, ... }
-	 */
-	filter(string: string = "", options: AutomodOptions = this.defaultOpts): AutomodReply {
-        const { langs, replacer, method } = { ...this.defaultOpts, ...options };
-        let reply: AutomodReply = {
-            time: Date.now(),
-            input: string,
-            output: string,
-            matches: [],
-            clean: true 
-        }
-        reply = this.clean(reply, { langs, replacer, method });
-        reply.time = Date.now() - reply.time;
-        return reply;
-	}
+    return reply;
+  }
 
-    private clean(reply: AutomodReply, options: AutomodOptions): AutomodReply {
-        const isProfane = this.methods.get(options.method);
-        for(let word of reply.output.split(/ /)) {
-            if (isProfane ? isProfane(word, options.langs) : false) {
-                reply.matches.push(word);
-                reply.clean = false;
-                reply.output = reply.output.replace(word, options.replacer.repeat(word.length));
-            }
+  private found(string: string, lang: any, index: number = 0): boolean {
+    const replacers: string[] | undefined =
+      lang.replacers[string.charAt(index)];
+    if (replacers) {
+      const results: boolean[] = [];
+      for (let i = 0; i < replacers.length; i++) {
+        const newString = string.replace(string.charAt(index), replacers[i]);
+        if (
+          lang.include.filter(
+            (w: string) => newString === w || newString.includes(w),
+          ).length != 0
+        ) {
+          return true;
         }
-        return reply;
-    }
-
-    private found(string: string, lang: any, index: number = 0): boolean {
-        let replacers: string[] | undefined = lang.replacers[string.charAt(index)];
-        if (replacers) {
-            let results: boolean[] = []
-            for(let i = 0; i < replacers.length; i++) {
-                let newString = string.replace(string.charAt(index), replacers[i]);
-                if (lang.include.filter((w: string) => newString === w || newString.includes(w)).length != 0) {
-                    return true;
-                }
-                if (index + 1 == string.length) {
-                    return false;
-                } else {
-                    results.push(this.found(newString, lang, index + 1));
-                }
-            }
-            return results.includes(true);
+        if (index + 1 == string.length) {
+          return false;
         } else {
-            if (index + 1 == string.length) {
-                return false;
-            } else {
-                return this.found(string, lang, index + 1);
-            }
+          results.push(this.found(newString, lang, index + 1));
         }
+      }
+      return results.includes(true);
+    } else {
+      if (index + 1 == string.length) {
+        return false;
+      } else {
+        return this.found(string, lang, index + 1);
+      }
     }
+  }
 }
